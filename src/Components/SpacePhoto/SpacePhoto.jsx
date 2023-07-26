53c0d275c2d370ba05db975cb2a1c9d8bb794dfe
Datsunco <danya200375d@gmail.com>
2023-07-26 21:12:46 +0300

    tg connection v2

--- src/Components/SpacePhoto/SpacePhoto.jsx
+++ src/Components/SpacePhoto/SpacePhoto.jsx
@@ -1,12 +1,9 @@
-import React, {useCallback,useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './SpacePhoto.css'
-//import Map from "../Map/Map";
-//import * as ymaps3 from 'ymaps3';
-//import {useState} from "react";
 
 
 const SpacePhoto = () => {
-    const [loaded, setLoaded] = useState(true);
+    //const [loaded, setLoaded] = useState(true);
     /*const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: "AIzaSyD8jQRBkxYYsQb6FWMPNjgSQW1lVIEj1EA"
