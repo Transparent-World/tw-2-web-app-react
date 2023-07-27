d0a74372f303516072d236aef7bae5bc504fdc11
Datsunco <danya200375d@gmail.com>
2023-07-27 22:37:46 +0300

    MainPage ButtonBlock & OrderHistory init

---
+++ src/Components/MainPage/ButtonsBlock/ButtonsBlock.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonBlock.css'
+
+const ButtonsBlock = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ButtonsBlock;
---
+++ src/Components/MainPage/ButtonsBlock/OrderButton/OrderButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OrderButton.css'
+
+const OrderButton = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default OrderButton;
