0f9a6b350654ddaf25632fc85135f1952d7083f1
Datsunco <danya200375d@gmail.com>
2023-08-03 22:00:02 +0300

    MainPage Search develop v5

--- src/Components/SearchBlock/SearchBlock.jsx
+++ src/Components/SearchBlock/SearchBlock.jsx
@@ -5,9 +5,11 @@ const SearchBlock = () => {
     return (
         <div className={'SearchBlock'}>
             <div className='search'>
-                <input className='search_input' placeholder="Введите адрес">
-                   
-                </input>
+                <svg className='icon' xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33" fill="none">
+                    <path d="M15.775 2C23.3875 2 29.55 8.1625 29.55 15.775C29.55 23.3875 23.3875 29.55 15.775 29.55C8.1625 29.55 2 23.3875 2 15.775C2 10.41 5.0595 5.77 9.54 3.4935" stroke="#292D32" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M31 30.9999L28.1 28.0999" stroke="#292D32" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <input className='search_input' placeholder="Введите адрес" />
             </div>
             <div className='location_variants'>
                 <div className='geolocation'>
