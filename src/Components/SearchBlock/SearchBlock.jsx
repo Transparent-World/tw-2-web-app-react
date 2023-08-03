5d72940339bead7d98c253fac8f2305d7fa278d6
Datsunco <danya200375d@gmail.com>
2023-08-03 23:39:14 +0300

    MainPage Search Animation develop v8

--- src/Components/SearchBlock/SearchBlock.jsx
+++ src/Components/SearchBlock/SearchBlock.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBlock.css'
 
 const SearchBlock = () => {
+
+    const changeHeight = () => {
+        let resizable = document.getElementById('SearchBlock');
+      
+        resizable.style.height = '80vh';
+        resizable.style.marginTop = '20vh';
+        console.log('resized')
+    }
     return (
-        <div className={'SearchBlock'}>
-            <div className='search'>
+        <div className={'SearchBlock'} id = 'SearchBlock'>
+            <div  className='search'>
                 <svg className='icon' xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33" fill="none">
                     <path d="M15.775 2C23.3875 2 29.55 8.1625 29.55 15.775C29.55 23.3875 23.3875 29.55 15.775 29.55C8.1625 29.55 2 23.3875 2 15.775C2 10.41 5.0595 5.77 9.54 3.4935" stroke="#292D32" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M31 30.9999L28.1 28.0999" stroke="#292D32" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
-                <input className='search_input' placeholder="Введите адрес" />
+                <input onClick={changeHeight}className='search_input' placeholder="Введите адрес" />
             </div>
             <div className='location_variants'>
                 <div className='geolocation'>
