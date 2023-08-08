4b19b0a0e219143148161f3b700fd4669fa3b511
Datsunco <danya200375d@gmail.com>
2023-08-08 17:55:35 +0300

    MapPage Current location develop  v28

--- src/Components/CurrentLocation/CurrentLocation.jsx
+++ src/Components/CurrentLocation/CurrentLocation.jsx
@@ -14,9 +14,9 @@ const CurrentLocation = () => {
 
     return (
         <div className='CurrentLocation'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-                <circle cx="15" cy="15" r="15" fill="#D9D9D9" />
-                <path d="M14.4133 15.9442L14.3757 15.5199L13.9508 15.4895L7.96613 15.0628L20.9764 9.02615L14.9565 22.0845L14.4133 15.9442Z" fill="#1D2733" stroke="#292D32" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38" fill="none">
+                <circle cx="19" cy="19" r="19" fill="#D9D9D9" />
+                <path d="M18.124 20.2078L18.0864 19.7834L17.6615 19.7531L9.56612 19.1758L26.843 11.1595L18.8562 28.4845L18.124 20.2078Z" fill="#1D2733" stroke="#292D32" />
             </svg>
         </div>
     );
