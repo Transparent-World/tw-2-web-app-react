ad27f93ee422bc8b55aa0a23ac797e7f34cb8cb9
Datsunco <danya200375d@gmail.com>
2023-08-24 21:07:17 +0300

    OrderHistory develop

--- src/storeData/OrderStore.js
+++ src/storeData/OrderStore.js
@@ -2,35 +2,7 @@ import { makeAutoObservable } from "mobx";
 
 export default class OrderStore {
     constructor(){
-        this._orders = [
-            {
-                id: 1,
-                userid: '736466798',
-                lon: '23.45',
-                lat: '23.45',
-                address: 'Мытищи 1',
-                radius: '200',
-                status: 0,
-            },
-            {
-                id: 2,
-                userid: '736466798',
-                lon: '23.45',
-                lat: '23.45',
-                address: 'Мытищи 2 ',
-                radius: '200',
-                status: 0,
-            },
-            {
-                id: 3,
-                userid: '736466798',
-                lon: '23.45',
-                lat: '23.45',
-                address: 'Мытищи 3 ',
-                radius: '200',
-                status: 0,
-            }
-        ]
+        this._orders = []
         makeAutoObservable(this)
     }
 
