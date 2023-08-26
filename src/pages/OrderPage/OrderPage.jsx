f326aad97f568589ab95b782d56247c1dab8a24f
Datsunco <danya200375d@gmail.com>
2023-08-26 20:31:47 +0300

    OrderPage develop v25

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -3,31 +3,49 @@ import {useLocation} from 'react-router-dom';
 import ChannelLink from '../../Components/ChannelLink/ChannelLink';
 import './OrderPage.css'
 import { fetchOrder } from '../../http/orderApi';
-import mapboxgl from 'mapbox-gl';
+import {mapboxgl, mapboxSdk} from 'mapbox-gl';
 
 const OrderPage = () => {
     const location = useLocation();
+    const [imgUrl, setImgUrl] = useState('')
     const [zoom, setZoom] = useState(9);
     const [order, setOrder] = useState({})
-    const mapContainer = React.useRef(null);
-    const map = React.useRef(null);
+    const mapboxClient = mapboxSdk({ accessToken: 'pk.eyJ1Ijoib3Rzb2Rpa292IiwiYSI6ImNsbDJzbGJ1eTA1cXgzaHF0amExd3RsbmcifQ.WVnp48kxoCMLuKjaCRD2hQ' });
 
 
     useEffect(() => {
         fetchOrder(location.state.id).then(resp => setOrder(resp.data[0]))
-        if (map.current || order == {}) return; // initialize map only once
+        /*if (map.current) return; // initialize map only once
         console.log(order)
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v12',
             center: [order.lon, order.lat],
             zoom: zoom
-        });
+       });
+        */
        }, [])
+
+    const onOrderReceived = useCallback((lon, lat) => {
+        const request = mapboxClient.static.getStaticImage({
+            ownerId: 'mapbox',
+            styleId: 'light-v10',
+            width: 500,
+            height: 350,
+            position: {
+            coordinates: [lon, lat],
+            zoom: 12
+            },
+            });
+        setImgUrl(request.url());
+        
+    }, [location])
 
     return (
         <div>
-            <div ref={mapContainer} className="map-container"/>
+            <div className='mapimg'>
+                {imgUrl}
+            </div>
             <div className='link'>
             <ChannelLink />
             </div>
