83c77eaa7b2c85a617486d81efb80ed5d3ed86ed
Datsunco <danya200375d@gmail.com>
2023-08-26 19:14:45 +0300

    OrderPage develop v19

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -6,16 +6,24 @@ import { fetchOrder } from '../../http/orderApi';
 const OrderPage = () => {
     const location = useLocation();
     const [order, setOrder] = useState({})
+    const mapContainer = React.useRef(null);
+    const map = React.useRef(null);
 
 
     useEffect(() => {
-        fetchOrder(location.state.id).then(resp => setOrder(resp.data[0])) 
-        console.log(location.state.id)
-        console.log(order)
+        fetchOrder(location.state.id).then(resp => setOrder(resp.data[0]))
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [order.lon, order.lat],
+            zoom: zoom
+        });
        }, [])
 
     return (
         <div>
+            <div ref={mapContainer} className="map-container"/>
             {order.id}
         </div>
     );
