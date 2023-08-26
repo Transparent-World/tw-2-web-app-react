161730d27a4b24a54b3e8bebfc9cfce9209a82af
Datsunco <danya200375d@gmail.com>
2023-08-26 21:00:20 +0300

    OrderPage develop v29

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -25,10 +25,6 @@ const OrderPage = () => {
         */
        }, [])
 
-    const onOrderReceived = useCallback((lon, lat) => {
-        
-        
-    }, [location])
 
     return (
         <div>
