e24e38e29a6a3a0ecb222b500bfd3805dfa0a100
Datsunco <danya200375d@gmail.com>
2023-08-26 17:43:09 +0300

    OrderPage develop v10

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -5,11 +5,19 @@ import { fetchOrder } from '../../http/orderApi';
 
 const OrderPage = () => {
     const location = useLocation();
-    const [order, setOrder] = useState()
+    const [order, setOrder] = useState({
+        id: 1,
+        userid: '736466798',
+        lon: '23.45',
+        lat: '23.45',
+        address: 'Мытищи 1',
+        radius: '200',
+        status: 0,
+    })
 
 
     useEffect(() => {
-        fetchOrder(location.state.id).then(resp => setOrder(resp))  
+        fetchOrder(location.state.id).then(resp => setOrder(resp.data))  
         console.log(location.state.id)
         console.log(order)
        }, [])
