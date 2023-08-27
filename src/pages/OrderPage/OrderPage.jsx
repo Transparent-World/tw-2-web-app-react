17ebb4f6dc2e6c1275863bce85d6098eb557634a
Datsunco <danya200375d@gmail.com>
2023-08-27 20:03:43 +0300

    OrderPage develop v30

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,10 +1,15 @@
-import React, {useEffect,useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import {useLocation} from 'react-router-dom';
 import ChannelLink from '../../Components/ChannelLink/ChannelLink';
 import './OrderPage.css'
 import { fetchOrder } from '../../http/orderApi';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3Rzb2Rpa292IiwiYSI6ImNsbDJzbGJ1eTA1cXgzaHF0amExd3RsbmcifQ.WVnp48kxoCMLuKjaCRD2hQ';
 
 const OrderPage = () => {
+    const mapContainer = React.useRef(null);
+    const map = React.useRef(null);
     const location = useLocation();
     const [imgUrl, setImgUrl] = useState('')
     const [zoom, setZoom] = useState(9);
@@ -13,22 +18,34 @@ const OrderPage = () => {
 
     useEffect(() => {
         //fetchOrder(location.state.id).then(resp => setOrder(resp.data[0]))
-        fetchOrder(location.state.id).then(resp => setOrder(resp.data[0]))
-        /*if (map.current) return; // initialize map only once
+        fetchOrder(location.state.id).then(resp => onDataReceived(resp.data[0]))
+        if (map.current) return; // initialize map only once
         console.log(order)
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v12',
-            center: [order.lon, order.lat],
+            center: [37.36, 55.45],
             zoom: zoom
        });
-        */
-       }, [])
+    }, [])
+
+    const onDataReceived = useCallback((data) => {
+        setOrder(data)
+        console.log('order loaded')
+        if (coordinates) {
+            console.log(JSON.stringify(coordinates))
+            map.current.flyTo({
+                center: [data.lon, data.lat]
+            });
+        }
 
 
+    }, [location])
+
+       //<img className='mapimg' src={'https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/'+order.lon+','+order.lat+',9,0/300x200?access_token=pk.eyJ1Ijoib3Rzb2Rpa292IiwiYSI6ImNsbDJzbGJ1eTA1cXgzaHF0amExd3RsbmcifQ.WVnp48kxoCMLuKjaCRD2hQ'}/>
     return (
         <div>
-            <img className='mapimg' src='https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/-121.3447,38.5826,9,0/300x200?access_token=pk.eyJ1Ijoib3Rzb2Rpa292IiwiYSI6ImNsbDJzbGJ1eTA1cXgzaHF0amExd3RsbmcifQ.WVnp48kxoCMLuKjaCRD2hQ'/>
+            <div ref={mapContainer} className="map-container"/>
             <div className='link'>
             <ChannelLink />
             </div>
