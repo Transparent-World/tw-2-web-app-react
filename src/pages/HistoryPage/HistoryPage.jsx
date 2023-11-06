5584d22f111b41dffe7b14a13c3a3df19c5d9606
DatsunCo <danya200375d@gmail.com>
2023-11-07 00:07:04 +0300

    add History Page with images

--- src/pages/HistoryPage/HistoryPage.jsx
+++ src/pages/HistoryPage/HistoryPage.jsx
@@ -8,13 +8,27 @@ import './HistoryPage.css'
 
 
 const HistoryPage = () => {
+    const tg = window.Telegram.WebApp;
+
     const { order } = useContext(Context)
 
+    const backButtonClicked = () => {
+        navigate("/mainpage");
+        tg.BackButton.hide();
+        navigate(0);
+    }
+
+    useEffect(() => {
+        tg.onEvent('backButtonClicked', backButtonClicked)
+        return () => {
+            tg.offEvent('backButtonClicked', backButtonClicked)
+        }
+    }, [])
     return (
         <div className='history_page'>
             <div className='header_history_page'>
                 <ChannelLink />
-                <div className='OrderHistory_text' >История заказов</div>
+                <div className='OrderHistory2_text' >История заказов</div>
             </div>
             <div className='orders2_block'>
                 {order.orders.toReversed().map(order =>
